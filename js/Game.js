f1a6c2e2565f0c03c8e34afa35197483499993cb
kpacheco84 <45826075+kpacheco84@users.noreply.github.com>
2019-02-11 11:49:49 -0500

    Initial commit

---
+++ js/Game.js
@@ -0,0 +1,16 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+class   Game{
+
+constructor(){
+
+this.missed = 0
+this.activePhrase = null
+this.phrases = [/*new phrase('sample phrase 2'),
+                new phrase('sample phrase 3'),
+                new phrase('sample phrase 4'),
+new phrase('sample phrase 5')*/]
+            }
+}
