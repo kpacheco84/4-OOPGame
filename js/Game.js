b78630fcbe3e54c8710d7818dbca6896613881f6
kpacheco84 <45826075+kpacheco84@users.noreply.github.com>
2019-02-11 16:17:40 -0500

    adding phrases and random picker

--- js/Game.js
+++ js/Game.js
@@ -8,9 +8,57 @@ constructor(){
 
 this.missed = 0
 this.activePhrase = null
-this.phrases = [/*new phrase('sample phrase 2'),
-                new phrase('sample phrase 3'),
-                new phrase('sample phrase 4'),
-new phrase('sample phrase 5')*/]
-            }
+this.phrases = [];
+}
+
+
+    /**
+
+     *  Creates phrases for use in game 
+
+     * @return {array} An array of phrases that could be used in the game 
+
+     */ 
+
+
+
+    createPhrase() {
+
+        const phrases = [];
+
+        phrases.push(new Phrase('First play a game'));
+
+        phrases.push(new Phrase('Three strikes ur out'));
+
+        phrases.push(new Phrase('Thirthy seconds'));
+
+        phrases.push(new Phrase('Fourth of july'));
+
+       
+
+        return phrases;
+
+ 
+
+    }
+
+    /**
+
+    * Selects random phrase from phrases property
+
+    * @return {Object} Phrase object chosen to be used
+
+    */
+
+    getRandomPhrase() {
+
+        const randNum = Math.floor(Math.random() * this.phrases.length); 
+
+        const randPhrase = this.phrases[randNum];
+
+        return randPhrase;
+
+    }
+             
+
 }
--- js/app.js
+++ js/app.js
@@ -2,5 +2,12 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const phrase = new Phrase();
-const game = new Game();
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+    };
+    const game = new Game();
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
+    logPhrase(game.getRandomPhrase());
