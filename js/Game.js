cc86e482bb56baf72e153177eb4bcd38c8fe9d17
kpacheco84 <45826075+kpacheco84@users.noreply.github.com>
2019-02-11 22:53:39 -0500

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,64 +1,37 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+	constructor() {
+		this.missed = 0
+		this.activePhrase = null
+		this.phrases = this.createPhrase();
+	}
+	/**
 
-class   Game{
+	 *  Creates phrases for use in game 
 
-constructor(){
+	 * @return {array} An array of phrases that could be used in the game 
 
-this.missed = 0
-this.activePhrase = null
-this.phrases = [];
-}
+	 */
+	createPhrase() {
+		const phrases = [];
+		phrases.push(new Phrase('First play a game'));
+		phrases.push(new Phrase('Three strikes ur out'));
+		phrases.push(new Phrase('Thirthy seconds'));
+		phrases.push(new Phrase('Fourth of july'));
+		return phrases;
+	}
+	/**
 
+	* Selects random phrase from phrases property
 
-    /**
+	* @return {Object} Phrase object chosen to be used
 
-     *  Creates phrases for use in game 
-
-     * @return {array} An array of phrases that could be used in the game 
-
-     */ 
-
-
-
-    createPhrase() {
-
-        const phrases = [];
-
-        phrases.push(new Phrase('First play a game'));
-
-        phrases.push(new Phrase('Three strikes ur out'));
-
-        phrases.push(new Phrase('Thirthy seconds'));
-
-        phrases.push(new Phrase('Fourth of july'));
-
-       
-
-        return phrases;
-
- 
-
-    }
-
-    /**
-
-    * Selects random phrase from phrases property
-
-    * @return {Object} Phrase object chosen to be used
-
-    */
-
-    getRandomPhrase() {
-
-        const randNum = Math.floor(Math.random() * this.phrases.length); 
-
-        const randPhrase = this.phrases[randNum];
-
-        return randPhrase;
-
-    }
-             
-
+	*/
+	getRandomPhrase() {
+		const randNum = Math.floor(Math.random() * this.phrases.length);
+		const randPhrase = this.phrases[randNum];
+		return randPhrase;
+	}
 }
